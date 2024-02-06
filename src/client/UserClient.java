55c6d334d270e2c90a2fd000778a8fd61579d72f
skander23000 <skanderchouiter@gmail.com>
2024-02-06 16:27:43 +0100

    mistake

--- src/client/UserClient.java
+++ src/client/UserClient.java
@@ -7,7 +7,7 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
 
-public class UserClient implements Remote {
+public class UserClient {
 
     private User user;
 
